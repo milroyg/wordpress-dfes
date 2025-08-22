758a0afbdfd687ce49364eb325f31ee86f0aca09
Milroy Gomes <108455817+milroyg@users.noreply.github.com>
2025-08-23 01:30:44 +0530

    Deleted unnecessary plugins

--- wp-content/plugins/pdf-embed/build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '74fa047eb33e37ef7c64');
--- wp-content/plugins/simply-static/src/admin/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => '3dfae64c2585d0b41a62');
