0252c6b1c0c280591d74bf425d7497271b0b06af
Alan Lobo <alansaviolobo@gmail.com>
2025-09-27 10:45:46 +0530

    trying to deregister the old bootstrap js file

--- wp-content/themes/xevso-child/functions.php
+++ wp-content/themes/xevso-child/functions.php
@@ -1,7 +1,6 @@
 <?php
 // Load parent theme stylesheet
 add_action('wp_enqueue_scripts', function () {
-    wp_enqueue_style('xevso-parent-style', get_template_directory_uri() . '/style.css');
+  wp_enqueue_style('xevso-parent-style', get_template_directory_uri() . '/style.css');
+  wp_deregister_script(get_template_directory_uri() . '/assets/bootstrap/bootstrap-min.js');
 });
-
-
