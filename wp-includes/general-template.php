5b718ee21eb21e52aa9cc10d58b1fc12fb4ff8ba
Milroy Gomes <108455817+milroyg@users.noreply.github.com>
2025-12-15 13:27:35 +0530

    accessibilty issues fix.

--- wp-includes/general-template.php
+++ wp-includes/general-template.php
@@ -4729,7 +4729,7 @@ function paginate_links( $args = '' ) {
 		$link .= $args['add_fragment'];
 
 		$page_links[] = sprintf(
-			'<a class="prev page-numbers" href="%s">%s</a>',
+			'<a class="prev page-numbers"  aria-label="Previous page" href="%s">%s</a>',
 			/**
 			 * Filters the paginated links for the given archive pages.
 			 *
@@ -4785,7 +4785,7 @@ function paginate_links( $args = '' ) {
 		$link .= $args['add_fragment'];
 
 		$page_links[] = sprintf(
-			'<a class="next page-numbers" href="%s">%s</a>',
+			'<a class="next page-numbers" aria-label="Next Page" href="%s">%s</a>',
 			/** This filter is documented in wp-includes/general-template.php */
 			esc_url( apply_filters( 'paginate_links', $link ) ),
 			$args['next_text']
