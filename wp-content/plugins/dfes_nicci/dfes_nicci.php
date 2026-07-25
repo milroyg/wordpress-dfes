0edca0de183a60d57b57822da4e7df54f40690e4
Alan Lobo <alansaviolobo@gmail.com>
2026-07-25 23:56:57 +0530

    Adding the nicci chatbot.

---
+++ wp-content/plugins/dfes_nicci/dfes_nicci.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Plugin Name: DFES Nicci
+ * Description: Integrates the Nicci Chatbot into WordPress.
+ * Version: 1.0
+ * Author: Junie
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
+
+/**
+ * Enqueue scripts.
+ */
+function dfes_nicci_enqueue_scripts() {
+    // Enqueue the external Nicci script. 
+    // We assume WordPress's built-in jQuery is sufficient.
+    wp_enqueue_script( 'nicci-js', 'https://niccicms.raj.nic.in/nicci/js/nicci.js', array( 'jquery' ), '1.0', true );
+
+    // Add the initialization logic from chat.html
+    $inline_script = "
+    jQuery(document).ready(function () {
+        jQuery.ajax({
+            type: 'POST',
+            url: 'https://niccicms.raj.nic.in/nicci/chatbotService.asmx/chatbot_nicci',
+            data: \"{authtoken: 'Gst2O6E2cpSOnGOB'}\",
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            success: function (c) {
+                var chatbotcode = c.d;
+                // The original code replaces a specific jQuery URL to prevent it from loading/conflicting
+                chatbotcode = chatbotcode.replace('https://niccicms.raj.nic.in/nicci/nicci/js/jquery.min.js', 'https://niccicms.raj.nic.in/nicci/nicci/js/jquery.min.js1');
+                chatbotcode = chatbotcode.replaceAll('$(', 'jQuery(');
+                jQuery('#ContentNicci').append(chatbotcode);
+            }
+        });
+    });";
+    wp_add_inline_script( 'nicci-js', $inline_script );
+}
+add_action( 'wp_enqueue_scripts', 'dfes_nicci_enqueue_scripts' );
+
+/**
+ * Register shortcode [dfes_nicci]
+ */
+function dfes_nicci_shortcode() {
+    return '<div id="ContentNicci"></div>';
+}
+add_shortcode( 'dfes_nicci', 'dfes_nicci_shortcode' );
